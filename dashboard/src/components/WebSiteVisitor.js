0f02949eaeff2a192eeeb8b6a1da4ded57eb9f39
Rainier <rainier.1123@gmail.com>
2022-10-18 09:57:41 -0400

    done

---
+++ dashboard/src/components/WebSiteVisitor.js
@@ -0,0 +1,13 @@
+//import WebSiteBox from './WebSiteBox'
+function WebSiteVisitor(props){
+  return(
+      <div className='website'>
+          <div className='left'>
+          <h3>Website Visitors</h3><br></br>
+          <h3>{props.list}</h3>
+          </div>
+          <div className='empty'></div>
+      </div>
+  )
+}
+export default WebSiteVisitor
